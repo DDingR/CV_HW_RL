d6613cbd11b11687657115aa5d286e9aaa315d80
DDingR <msryu00@gmail.com>
2022-04-10 19:53:26 +0900

    env add , argparse add, docs add

--- PPO_main.py
+++ PPO_main.py
@@ -1,37 +1,47 @@
+from re import A
 from mlagents_envs.environment import UnityEnvironment, ActionTuple
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
 
-import sys
-from PPO_agent import Agent
+import argparse
+from DNN_PPO_agent import DNN_PPO_Agent
+from CNN_PPO_agent import CNN_PPO_Agent
+from CV_feature_extractor import *
 import numpy as np
 from datetime import datetime
-import cv2
 
 def shape_check(array, shape):
     assert array.shape == shape, \
         'shape error | array.shape ' + str(array.shape) + ' shape: ' + str(shape)
 
-def main():
-    if len(sys.argv) == 1:
-        file_name = './CarDrive.x86_64'
-    else:
-        file_name = sys.argv[1]
+def main(args):
+    if 'args.load_network_path' in locals():
+        raise Exception('not ready')
+
     # 환경 정의 및 설정 
     engine_configuration_channel = EngineConfigurationChannel()
-    env = UnityEnvironment(file_name, 
+    env = UnityEnvironment(args.env_path, 
                            worker_id=np.random.randint(65535),
                            side_channels=[engine_configuration_channel])
     env.reset()
-    print("after reset")
+
     # agent
-    state_dim = 9 # need to check
-    action_dim = 2 # xz
+    action_dim = 2 # xyz Quart...
     action_bound = 1 # max_input 
-    agent = Agent(state_dim, action_dim, action_bound)
+    if args.case_num ==  0:
+        state_dim = 8 # not defined
+        agent = DNN_PPO_Agent(state_dim, action_dim, action_bound, args)
+    elif args.case_num ==  1:
+        state_dim = 3 # not defined
+        agent = DNN_PPO_Agent(state_dim, action_dim, action_bound, args)
+    elif args.case_num == 2:
+        state_dim = (84, 84, 3) # not defined
+        agent = CNN_PPO_Agent(state_dim, action_dim, action_bound, args)
+    else:
+        raise Exception("check case_num you trying (sould be in range[0:2]")
 
     # behavior 이름 불러오기 및 timescale 설정
     behavior_name = list(env.behavior_specs)[0]
-    engine_configuration_channel.set_configuration_parameters(time_scale=20.0)
+    engine_configuration_channel.set_configuration_parameters(time_scale=args.time_scale)
 
     score_list = []
     max_score = 1e-9
@@ -44,18 +54,11 @@ def main():
 
         # decision_steps와 terminal_steps 정의
         decision_steps, terminal_steps = env.get_steps(behavior_name)
-        
-        print(np.array(decision_steps.obs[0]).shape)
-        print(np.array(decision_steps.obs[1]).shape)
-        print(np.array(decision_steps.obs[0][0]).shape)        
-
-        img = decision_steps.obs[0][0]
-        cv2.imshow('image', img)
-        cv2.waitKey()
-        cv2.destroyAllWindows()      
 
         # state
         state = decision_steps.obs[0][0] # need to check
+        if args.case_num == 1:
+            state = extract(state)
         state = np.reshape(state, [1, state_dim])
 
         # 파라미터 초기화 
@@ -81,54 +84,99 @@ def main():
             # 행동 수행 후 에이전트의 정보 (상태, 보상, 종료 여부) 취득
             decision_steps, terminal_steps = env.get_steps(behavior_name)
             
+            # done, reward check
             done = len(terminal_steps.agent_id)>0
             reward = terminal_steps.reward[0] if done else decision_steps.reward[0]
 
+            # next_state
             if done:
-                # next_state = [terminal_steps.obs[i][0] for i in range(6)]
                 next_state = terminal_steps.obs[0][0]
             else:
-                # next_state = [decision_steps.obs[i][0] for i in range(6)]
                 next_state = decision_steps.obs[0][0]
-            # 매 스텝 보상을 에피소드에 대한 누적보상에 더해줌 
-            
+
+            if args.case_num == 1:
+                next_state = extract(next_state)
+
+            # reshaping            
             action = np.reshape(action, [1, action_dim])
             next_state = np.reshape(next_state, [1, state_dim])
             reward = np.reshape(reward, [1, 1])
             done = np.reshape(done , [1, 1])
 
-            agent.sample_append(
-                state,
-                action,
-                reward,
-                next_state,
-                done
-            )
+            # train
+            if args.train:
+                agent.sample_append(
+                    state,
+                    action,
+                    reward,
+                    next_state,
+                    done
+                )
+                agent.train()
 
+            # for next_step
             score += reward 
             state = next_state
-            # print(state)
-            # print(action)
         
+        # score store
         score = score[0][0]
         score_list.append(score)
 
-        print(
-            'EPISODE: ', e+1,
-            'STEP: ', step,
-            'SCORE: ', round(score, 3),
-        )
+        if args.train:
+            # report train result
+            print(
+                'EPISODE: ', e+1,
+                'STEP: ', step,
+                'SCORE: ', round(score, 3),
+            )
+            agent.draw_tensorboard(score, step, e)
 
-        agent.draw_tensorboard(score, step, e)
+            # update weights
+            if score > max_score:
+                max_score = score
+                now = datetime.now()
+                now = now.strftime('%m%d%H%M')
+                save_name = './trained_model/' + args.train_name + '/' +  now + 'EPISODE' + str(e+1)
 
-        if score > max_score:
-            max_score = score
-            now = datetime.now()
-            now = now.strftime('%m%d%H%M')
-            agent.Actor_model.save_weights('DDPG' + now)
+                agent.Actor_model.save_weights(save_name)
 
     # 환경 종료 
     env.close() 
 
 if __name__=='__main__':
-    main()
+    now = datetime.now()
+    now = now.strftime('%m%d%H%M')
+
+    parser = argparse.ArgumentParser(description="choice options")
+    parser.add_argument('--train_name', type=str, 
+                        default=now, dest="train_name", action="store",
+                        help='trained model would be saved in that directory')
+    parser.add_argument('--env_path', type=str, 
+                        default='./envs/env1/env1.x86_64', dest="env_path", action="store",
+                        help='environment path e.g. --env_path \'./envs/...\'')
+    parser.add_argument('--case_num', type=int,
+                        default=0, dest='case_num', action='store',
+                        help='0: observation vector, 1: cv feature extractor, 2: CNN extractor')
+    parser.add_argument('--train',
+                        default=False, dest='train', action='store_true',
+                        help='add this arg to train')
+    parser.add_argument('--load_network', type=str,
+                        default='./NOT_LOADED', dest='load_network_path', action='store',
+                        help='to load trained network add path e.g. --load_network \'./...\'')
+    parser.add_argument('--time_scale', type=float, 
+                        default=20.0, dest='time_scale', action="store",
+                        help='to accellerate simul (consider you PC spec)')
+    args = parser.parse_args()
+
+    if args.train:
+        with open("./trained_model/train_history.txt", 'a') as f:
+            f.write(
+                "TRAIN INFO\n" + 
+                "train name: " + str(args.train_name) + '\n' + 
+                "env_path: " + str(args.env_path) + '\n' + 
+                "case_name: " + str(args.case_num) + '\n' + 
+                "load network: " + str(args.load_network_path) + '\n' +
+                "=================================================\n" 
+            )    
+
+    main(args)
